5fec7dc545c805f451859c60d91607aeca5e820b
valer <lainezvaleria18@gmail.com>
2026-05-03 22:21:34 -0600

    feat : Creación del controlador para la gestión de libros, Mónica Valeria Ruballo Laínez

---
+++ MVRLJDGA.WebApplication/Controllers/BookContoller.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MVRLJDGA.BusinessLogic.DTOs;
+using MVRLJDGA.BusinessLogic.UseCases.Books.Commands.CreateBook;
+using MVRLJDGA.BusinessLogic.UseCases.Books.Queries.GetBooks;
+
+namespace MVRLJDGA.WebApplication.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public BookController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var query = new GetBooksQuery();
+            var model = await _mediator.Send(query);
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(BookDto bookDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var command = new CreateBookCommand(bookDto);
+                await _mediator.Send(command);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookDto);
+        }
+    }
+}
