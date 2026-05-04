3f9b8f5d74a44639e054b188048c309dea27511f
valer <lainezvaleria18@gmail.com>
2026-05-03 22:29:23 -0600

    update : Configuración de  Program.cs - Mónica Valeria Ruballo Laínez

--- MVRLJDGA.WebApplication/Program.cs
+++ MVRLJDGA.WebApplication/Program.cs
@@ -1,15 +1,30 @@
 using MVRLJDGA.DataAccess;
 using MVRLJDGA.BusinessLogic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDataAccessLayer(builder.Configuration);
-builder.Services.AddBusinessLogicLayer(); 
+builder.Services.AddBusinessLogicLayer();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
