84a8bec689904f2813b450bb5154764acda2c153
valer <lainezvaleria18@gmail.com>
2026-05-05 16:34:07 -0600

    feat(BusinessLogic): agregar comando y handler para editar libros en el sistema, Monica Valeria Ruballo Laìnez

---
+++ MVRLJDGA.BusinessLogic/UseCases/Books/Commands/UpdateBook/UpdateBookCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using MVRLJDGA.BusinessLogic.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVRLJDGA.BusinessLogic.UseCases.Books.Commands.UpdateBook
+{
+    public record UpdateBookCommand(BookDto BookDto) : IRequest<bool>;
+}
---
+++ MVRLJDGA.BusinessLogic/UseCases/Books/Commands/UpdateBook/UpdateBookHandler.cs
@@ -0,0 +1,32 @@
+﻿using Mapster;
+using MediatR;
+using MVRLJDGA.DataAccess.Interfaces;
+using MVRLJDGA.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVRLJDGA.BusinessLogic.UseCases.Books.Commands.UpdateBook
+{
+    public class UpdateBookHandler : IRequestHandler<UpdateBookCommand, bool>
+    {
+        private readonly IEfRepository<Book> _bookRepository;
+
+        public UpdateBookHandler(IEfRepository<Book> bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<bool> Handle(UpdateBookCommand request, CancellationToken cancellationToken)
+        {
+            var book = await _bookRepository.GetByIdAsync(request.BookDto.Id);
+            if (book == null) return false;
+
+          
+            request.BookDto.Adapt(book);
+
+            await _bookRepository.UpdateAsync(book);
+            return true;
+        }
+    }
+}
