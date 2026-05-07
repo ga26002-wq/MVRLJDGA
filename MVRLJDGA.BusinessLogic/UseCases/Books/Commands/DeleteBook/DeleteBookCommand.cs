d24b956317ac18049b035b0e402fe52e7ea650d4
valer <lainezvaleria18@gmail.com>
2026-05-06 23:31:56 -0600

    feat(UseCases): implementaciòn de DeleteBookCommand y DeleteBookHandler , Mònica Valeria Ruballo Laìnez

---
+++ MVRLJDGA.BusinessLogic/UseCases/Books/Commands/DeleteBook/DeleteBookCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace MVRLJDGA.BusinessLogic.UseCases.Books.Commands.DeleteBook
+{
+    public class DeleteBookCommand : IRequest<bool>
+    {
+        public long Id { get; set; }
+
+        public DeleteBookCommand(long id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ MVRLJDGA.BusinessLogic/UseCases/Books/Commands/DeleteBook/DeleteBookHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using MVRLJDGA.DataAccess.Interfaces;
+using MVRLJDGA.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MVRLJDGA.BusinessLogic.UseCases.Books.Commands.DeleteBook
+{
+    public class DeleteBookHandler : IRequestHandler<DeleteBookCommand, bool>
+    {
+        private readonly IEfRepository<Book> _bookRepository;
+
+        public DeleteBookHandler(IEfRepository<Book> bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<bool> Handle(DeleteBookCommand request, CancellationToken cancellationToken)
+        {
+            var book = await _bookRepository.GetByIdAsync(request.Id);
+            if (book == null) return false;
+
+            await _bookRepository.DeleteAsync(book);
+            return true;
+        }
+    }
+}
