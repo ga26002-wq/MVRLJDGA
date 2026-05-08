ba9e16d339ab5b33932ea437770a3d63f202f218
valer <lainezvaleria18@gmail.com>
2026-05-08 06:07:31 -0600

    feat( Dtos) creacion de de UserDto : Monica Valeria Ruballo Laìnez

---
+++ MVRLJDGA.BusinessLogic/DTOs/UserDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVRLJDGA.BusinessLogic.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El rol es requerido para el usuario.")]
+        [Display(Name = "Rol asignado")]
+        public int RoleId { get; set; }
+
+        [Required(ErrorMessage = "El nombre de cuenta es requerido.")]
+        [StringLength(60, ErrorMessage = "El nombre de cuenta no puede superar los 60 caracteres.")]
+        [Display(Name = "Nombre de Cuenta")]
+        public string AccountName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La clave de acceso es requerida.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Clave de Acceso")]
+        public string AccessKey { get; set; } = string.Empty;
+
+        [Display(Name = "Nombre del Rol")]
+        public string RoleTitle { get; set; } = string.Empty;
+    }
+}
