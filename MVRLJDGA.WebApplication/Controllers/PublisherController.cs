66ea40ab4cadd42013ce638ec661eacdaef09ee1
valer <lainezvaleria18@gmail.com>
2026-05-08 05:55:23 -0600

    feat(Publisher): creacion de  controlador y creacion de editoriales , Monica Valeria Ruballo Laìnez

---
+++ MVRLJDGA.WebApplication/Controllers/PublisherController.cs
@@ -0,0 +1,48 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using MVRLJDGA.BusinessLogic.DTOs;
+using MVRLJDGA.DataAccess.Interfaces;
+using MVRLJDGA.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MVRLJDGA.WebApplication.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private readonly IEfRepository<Publisher> _publisherRepository;
+
+        public PublisherController(IEfRepository<Publisher> publisherRepository)
+        {
+            _publisherRepository = publisherRepository;
+        }
+
+     
+        public async Task<IActionResult> Index()
+        {
+            var publishers = await _publisherRepository.ListAsync();
+           
+            var model = publishers.Adapt<List<PublisherDto>>();
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PublisherDto publisherDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var publisher = publisherDto.Adapt<Publisher>();
+                await _publisherRepository.AddAsync(publisher);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(publisherDto);
+        }
+    }
+}
